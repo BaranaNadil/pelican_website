d9a7e28f8ad394c32c762b355772da2735aaf85b
Kovi <139319195+RogueChocoholic@users.noreply.github.com>
2024-08-20 19:44:28 +0530

    done
    added the cards
    added animations using gsap
    added gsap cdn
    gsap codes in script.js file
    added services cards images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,82 @@
-console.log("hello");
+document.addEventListener("DOMContentLoaded", (event) => {
+  var cards = document.querySelectorAll(".services-card");
+
+  cards.forEach((card) => {
+    // animating cards on hover
+    // card body
+    var cardLearn = card.querySelector(".service-text-learn");
+    var cardDesc = card.querySelector(".service-text-desc");
+    var cardItem = card.querySelector(".services-item");
+    var cardTitle = card.querySelector(".service-card-title");
+    card.addEventListener("mouseenter", () => {
+      gsap.to(card, {
+        duration: 0.25,
+        scale: 1,
+        zIndex: 3,
+        margin: "-50px",
+        ease: "back.out",
+        opacity: 1,
+        borderColor: "#002FFF"
+      });
+      gsap.to(cardItem, {
+        filter: "grayscale(0%) blur(0px)",
+        duration: 0.5,
+        ease: "back",
+      });
+      gsap.to(cardDesc, {
+        filter: "blur(0px)",
+        ease: "back",
+        delay: 0.4,
+        duration: 1,
+      });
+      gsap.to(cardLearn, {
+        filter: "blur(0px)",
+        ease: "back",
+        delay: 0.4,
+        duration: 2,
+      });
+      gsap.to(cardTitle,{
+        color: "#002FFF",
+        duration: 1
+      })
+    });
+
+    card.addEventListener("mouseleave", () => {
+      // animating cards after hover
+      // card body
+      gsap.to(card, {
+        duration: 0.25,
+        scale: 0.8,
+        zIndex: 1,
+        ease: "back.out",
+        margin: "-20",
+        opacity: 0.5,
+        borderColor: "white",
+      });
+      gsap.to(card.querySelector(".services-item"), {
+        filter: "grayscale() blur(5px)",
+        duration: 0.5,
+        ease: "power2.out",
+      });
+      gsap.to(cardDesc, {
+        filter: "blur(5px)",
+        ease: "back",
+        delay: 0.4,
+        duration: 1,
+      });
+      gsap.to(cardLearn, {
+        filter: "blur(5px)",
+        ease: "back",
+        delay: 0.4,
+        duration: 1,
+      });
+      gsap.to(cardTitle,{
+        color: "#0f014d",
+        duration: 0.5
+      })
+    });
+  });
+});
+
+// left to do
+// change the blur effect on all cards. add blur to text except for the titles change title colors on hover
